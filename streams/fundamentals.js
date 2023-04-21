b35200cffa5a86903bb2e3813da2d118a7062f59
Michel Junior <jrcassunde95@gmail.com>
2023-04-21 18:09:32 -0300

    implementando stream transform

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import {Readable, Writable} from 'node:stream'
+import {Readable, Writable, Transform} from 'node:stream'
 
 
 class OneToHundredStream extends Readable{
@@ -18,6 +18,13 @@ index = 1
     }
 }
 
+class InverseNumberStream extends Transform{
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1;
+        callback(null, Buffer.from(String(transformed)));
+    }
+}
+
 class MultiplyByTenStream extends Writable{
     _write(chunk, encoding, callback){
         console.log(Number(chunk.toString()) *10)
@@ -26,4 +33,5 @@ class MultiplyByTenStream extends Writable{
 }
 
 new OneToHundredStream()
+.pipe(new InverseNumberStream())
 .pipe(new MultiplyByTenStream())
